4855073fcd8f9523a59ddbe3312961db703fd923
Radu Vaida <111689573+raduv7@users.noreply.github.com>
2023-10-19 18:31:56 +0300

    fully tested

---
+++ lab1/src/test/java/labs/lab1/ApplicationTest.java
@@ -0,0 +1,11 @@
+package labs.lab1;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testApplicationStart() {
+        Application.main(new String[]{});
+    }
+}
