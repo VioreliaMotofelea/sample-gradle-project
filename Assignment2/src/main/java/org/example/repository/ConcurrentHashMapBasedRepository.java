f2ac7d3753693dc135d10be13142ff94b2177832
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-10-29 12:12:30 +0200

    created concurrenthashmap repo

---
+++ Assignment2/src/main/java/org/example/repository/ConcurrentHashMapBasedRepository.java
@@ -0,0 +1,28 @@
+package org.example.repository;
+
+import java.util.AbstractMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapBasedRepository<T> implements InMemoryRepository<T> {
+
+    private final AbstractMap<Integer, T> map;
+
+    public ConcurrentHashMapBasedRepository() {
+        map = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void add(T item) {
+        map.put(item.hashCode(), item);
+    }
+
+    @Override
+    public void remove(T item) {
+        map.remove(item.hashCode());
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return map.containsKey(item.hashCode());
+    }
+}
