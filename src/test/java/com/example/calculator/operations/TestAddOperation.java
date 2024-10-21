fe80e7afdf26271158b30d47b446b6b69069d93f
916-Suciu-Bogdan <bogdan.suciu1@stud.ubbcluj.ro>
2024-10-21 17:26:53 +0300

    Removed Warning

--- src/test/java/com/example/calculator/operations/TestAddOperation.java
+++ src/test/java/com/example/calculator/operations/TestAddOperation.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.assertEquals;
+
 
 public class TestAddOperation {
 
